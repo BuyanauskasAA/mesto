c45ddb15bb888c65597cf84fd487e4d517d416e2
BuyanauskasAA <Buyanauskas@gmail.com>
2023-03-25 19:57:02 +0300

    feat: добавление начального набора карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Эльбрус',
+    link: './images/elbrus.jpg'
+  },
+  {
+    name: 'Куршская коса',
+    link: './images/kurshskaya-kosa.jpg'
+  },
+  {
+    name: 'Рускеала',
+    link: './images/ruskeala.jpg'
+  },
+  {
+    name: 'Шиханы',
+    link: './images/shihany.jpg'
+  },
+  {
+    name: 'Кезенойам',
+    link: './images/kezenoyam.jpg'
+  },
+  {
+    name: 'Большой Тxач',
+    link: './images/thach.jpg'
+  }
+];
+
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
@@ -8,6 +35,25 @@ const profileAboutElement = document.querySelector('.profile__about');
 const popupFormElement = popupElement.querySelector('.popup__form');
 const popupInputNameElement = popupElement.querySelector('.popup__input_type_name');
 const popupInputAboutElement = popupElement.querySelector('.popup__input_type_about');
+
+const cardTemplate = document.querySelector('#element-template').content;
+const cardsContainer = document.querySelector('.elements__container');
+
+const renderCard = function (card) {
+  const title = card.name;
+  const link = card.link;
+  const cardElement = cardTemplate.cloneNode(true);
+
+  console.log(cardElement);
+
+  cardElement.querySelector('.element__title').textContent = title;
+  cardElement.querySelector('.element__image').src = link;
+  cardElement.querySelector('.element__image').alt = title;
+
+  cardsContainer.append(cardElement);
+}
+
+initialCards.forEach(renderCard);
 
 const openPopup = function () {
   popupInputNameElement.value = profileNameElement.textContent;
