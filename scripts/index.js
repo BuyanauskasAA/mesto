a226834d2771885bcc86228a9731f765e787426f
BuyanauskasAA <Buyanauskas@gmail.com>
2023-05-04 20:12:22 +0300

    fix: исправить импорт Card.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { Card } from "./card.js";
+import { Card } from "./Card.js";
 import { initialCards } from "./constants.js";
 import { FormValidator } from "./FormValidator.js";
 import { validationConfig } from "./validation-config.js";
