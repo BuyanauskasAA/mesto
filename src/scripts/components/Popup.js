ea8bb53f1c20965b1e209b8d8c98938709327fdd
BuyanauskasAA <Buyanauskas@gmail.com>
2023-05-10 13:47:00 +0300

    fix: вынести функцию закрытия по нажатию Esc с привязанным контекстом в отдельную именованную функцию

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -1,6 +1,7 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handlePopupEscClose = this._handleEscClose.bind(this);
   }
 
   _handleEscClose(event) {
@@ -10,12 +11,12 @@ export default class Popup {
   }
 
   open() {
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handlePopupEscClose);
     this._popup.classList.add('popup_opened');
   }
 
   close() {
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handlePopupEscClose);
     this._popup.classList.remove('popup_opened');
   }
 
