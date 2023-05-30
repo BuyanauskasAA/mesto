569c43b904243303f3a13c3442c16d940dadd826
BuyanauskasAA <Buyanauskas@gmail.com>
2023-05-31 00:25:48 +0300

    fix: удалить initialCards из constants.js

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -1,22 +1,3 @@
-const elbrusImage = new URL("../../images/elbrus.jpg", import.meta.url);
-const kurshskayaKosaImage = new URL(
-  "../../images/kurshskaya-kosa.jpg",
-  import.meta.url
-);
-const ruskealaImage = new URL("../../images/ruskeala.jpg", import.meta.url);
-const shihanyImage = new URL("../../images/shihany.jpg", import.meta.url);
-const kezenoyamImage = new URL("../../images/kezenoyam.jpg", import.meta.url);
-const thachImage = new URL("../../images/thach.jpg", import.meta.url);
-
-const initialCards = [
-  { name: "Эльбрус", link: elbrusImage },
-  { name: "Куршская коса", link: kurshskayaKosaImage },
-  { name: "Рускеала", link: ruskealaImage },
-  { name: "Шиханы", link: shihanyImage },
-  { name: "Кезенойам", link: kezenoyamImage },
-  { name: "Большой Тxач", link: thachImage },
-];
-
 const profilePopupElement = document.querySelector(".profile-popup");
 const profilePopupOpenButton = document.querySelector(".profile__edit-button");
 const profilePopupForm = profilePopupElement.querySelector(".popup__form");
